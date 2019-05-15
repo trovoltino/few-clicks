ec113cde861c0e7be7ab8d1efdf79921adaf08c3
matiiss-z <matiissdz@gmail.com>
2019-05-15 21:33:28 +0300

    work on form

--- src/components/pop-up.vue
+++ src/components/pop-up.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="container" v-on:click="show=!show" v-bind:class="{ hide: show }">
+  <div class="container" v-on:click="show=!show" v-bind:class="{ containerShow: show }">
     <form action="" v-bind:class="{ formShow: show }">
       <input type="text" placeholder="Name">
       <input type="email" name="email" id="email" placeholder="email">
       <textarea name="message" rows="10" cols="30">
           The cat was playing in the garden.
       </textarea>
-      <input type="button" value="submit">
+      <button>submit</button>
     </form>
     <p class="contact">contact us</p>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'pop-up',
   data() {
@@ -21,6 +22,7 @@ export default {
     }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -32,15 +34,19 @@ export default {
     transform: translate(-50%, 50%);
     height: 150px;
     width: 320px;
-    -moz-border-radius: 150px / 80px;
-    -webkit-border-radius: 150px / 80px;
-    border-radius: 150px / 80px;
-    background:rgb(63, 63, 63);
+    border-radius: 70px;
+    background:rgba(48, 48, 48, 0.8);
     transition: all 1s ease-in-out;
     border: solid black 1px;
-    box-shadow: 2px silver;
+    box-shadow:inset 0px 0px 0px 1px #bee2f9;
+    border:1px solid #b4b4b4;
+    cursor:pointer;
+    color:#14396a;
+    text-shadow:0px 1px 0px #dee0e2;
   }
-  
+  .btn-look {
+    box-shadow:inset 0px 0px 0px 1px #bee2f9;
+  }
   p {
     text-align: center;
     text-transform: uppercase;
@@ -56,14 +62,48 @@ export default {
   form {
     display: none;
     transition: all 1s ease-in-out;
+    position: relative;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-105%);
+    width: 70%;
+  }
+  form input, form textarea{
+    color: white;
+    border: white solid 2px;
+    border-radius: 10px;
+    background: transparent;
+    position: relative;
+    left: 50%;
+    transform: translate(-50%,0%);
+  }
+  button {
+    padding: 10px;
+    border-radius: 20px;
+    cursor: pointer;
+    margin: 10px 0px;
+    text-transform: uppercase;
+    font-weight: 700;
+    font-family: sans-serif;
+  }
+  input {
+    height: 30px;
+    text-align: center;
+    margin: 10px 0px;
+  }
+  textarea {
+    margin: 10px 0px;
   }
   .formShow {
     display: flex;
     flex-direction: column;
-    
+    justify-content: flex-start;
+  }
+  .containerShow {
+    height: 850px;
+    width: 650px;
   }
   .hide {
-    height: 75%;
-    width: 45%;
+    display: none;
   }
 </style>
